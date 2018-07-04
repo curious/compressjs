49147e6897c39aa9215130ea08d5190492b389b1
James Reed <jrreed84@gmail.com>
2018-07-03 22:18:24 -0700

    Remove amdefine and replace with require/module to support browserify

--- main.js
+++ main.js
@@ -1,29 +1,21 @@
-if (typeof define !== 'function') { var define = require('amdefine')(module); }
-define(['./lib/freeze','./lib/BitStream','./lib/Stream','./lib/BWT','./lib/Context1Model','./lib/DefSumModel','./lib/FenwickModel','./lib/MTFModel','./lib/NoModel','./lib/Huffman','./lib/RangeCoder','./lib/BWTC','./lib/Bzip2','./lib/Dmc','./lib/Lzjb','./lib/LzjbR','./lib/Lzp3','./lib/PPM','./lib/Simple'], function(freeze,BitStream,Stream,BWT,Context1Model,DefSumModel,FenwickModel,MTFModel,NoModel,Huffman,RangeCoder,BWTC,Bzip2,Dmc,Lzjb,LzjbR,Lzp3,PPM,Simple) {
-    'use strict';
-    return freeze({
-        version: "0.0.1",
-        // APIs
-        BitStream: BitStream,
-        Stream: Stream,
-        // transforms
-        BWT: BWT,
-        // models and coder
-        Context1Model: Context1Model,
-        DefSumModel: DefSumModel,
-        FenwickModel: FenwickModel,
-        MTFModel: MTFModel,
-        NoModel: NoModel,
-        Huffman: Huffman,
-        RangeCoder: RangeCoder,
-        // compression methods
-        BWTC: BWTC,
-        Bzip2: Bzip2,
-        Dmc: Dmc,
-        Lzjb: Lzjb,
-        LzjbR: LzjbR,
-        Lzp3: Lzp3,
-        PPM: PPM,
-        Simple: Simple
-    });
-});
+module.exports = {
+    BitStream: require('./lib/BitStream'),
+    Stream: require('./lib/Stream'),
+    BWT: require('./lib/BWT'),
+    Context1Model: require('./lib/Context1Model'),
+    DefSumModel: require('./lib/DefSumModel'),
+    FenwickModel: require('./lib/FenwickModel'),
+    MTFModel: require('./lib/MTFModel'),
+    NoModel: require('./lib/NoModel'),
+    Huffman: require('./lib/Huffman'),
+    RangeCoder: require('./lib/RangeCoder'),
+    // compression methods
+    BWTC: require('./lib/BWTC'),
+    Bzip2: require('./lib/Bzip2')
+    Dmc: require('./lib/Dmc'),
+    Lzjb: require('./lib/Lzjb'),
+    LzjbR: require('./lib/LzjbR'),
+    Lzp3: require('./lib/Lzp3'),
+    PPM: require('./lib/PPM'),
+    Simple: require('./lib/Simple')
+};
